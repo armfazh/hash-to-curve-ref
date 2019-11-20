d23f05c28cbbddc317e7bb4efa80be7f6a36da0d
armfazh <armfazh@cloudflare.com>
2019-11-20 15:02:10 -0800

    Remove constant non-required.

--- go-h2c/mapping/ell2A0/ell2A0.go
+++ go-h2c/mapping/ell2A0/ell2A0.go
@@ -13,9 +13,6 @@ type ell2A0 struct {
 	E      C.M
 	Sgn0   func(GF.Elt) int
 	AA, BB GF.Elt
-	cte    struct {
-		C1 *big.Int
-	}
 }
 
 func (m ell2A0) String() string { return fmt.Sprintf("Elligator2A0 for E: %v", m.E) }
@@ -40,18 +37,12 @@ func (m *ell2A0) verify() bool {
 }
 
 func (m *ell2A0) precmp(sgn0 GF.Sgn0ID) {
-	q := m.E.F.Order()
 	m.Sgn0 = m.E.F.GetSgn0(sgn0)
 
-	t0 := big.NewInt(1)
-	t0.Add(q, t0) // (q+1)
-	t0.Rsh(q, 2)  // (q+1)/4
-	m.cte.C1 = t0
-
 	F := m.E.F
-	t1 := F.Inv(m.E.B)      // 1/B
-	m.AA = F.Mul(t1, m.E.A) // A/B
-	m.BB = F.Sqr(t1)        // 1/B^2
+	t0 := F.Inv(m.E.B)      // 1/B
+	m.AA = F.Mul(t0, m.E.A) // A/B
+	m.BB = F.Sqr(t0)        // 1/B^2
 }
 
 func (m *ell2A0) MapToCurve(u GF.Elt) C.Point {
@@ -63,8 +54,8 @@ func (m *ell2A0) MapToCurve(u GF.Elt) C.Point {
 	x2 = F.Neg(x1)                 // 2.  x2 = -x1
 	gx1 = F.Sqr(x1)                // 3. gx1 = x1^2
 	gx1 = F.Add(gx1, m.BB)         // 4. gx1 = gx1 + B
-	gx1 = F.Mul(gx1, x1)           // 5. gx1 = gx1 * x1           // gx1 = x1^3 + B * x1
-	y = F.Sqrt(gx1)                // 6.   y = gx1^c1             // This is either sqrt(gx1) or sqrt(gx2)
+	gx1 = F.Mul(gx1, x1)           // 5. gx1 = gx1 * x1   // gx1 = x1^3 + B * x1
+	y = F.Sqrt(gx1)                // 6.   y = sqrt(gx1)  // This is either sqrt(gx1) or sqrt(gx2)
 	e1 = F.AreEqual(F.Sqr(y), gx1) // 7.  e1 = (y^2) == gx1
 	x = F.CMov(x2, x1, e1)         // 8.   x = CMOV(x2, x1, e1)
 	e2 = m.Sgn0(u) == m.Sgn0(y)    // 9.  e2 = sgn0(u) == sgn0(y)
